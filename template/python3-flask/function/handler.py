a592679951c1701b828ed6a378ba644cb0f81c4f
Dietrich Schulten <ds@escalon.de>
2019-04-03 11:42:59 +0200

    initial version

--- template/python3-flask/function/handler.py
+++ template/python3-flask/function/handler.py
@@ -1,7 +1,28 @@
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from flask import send_file
+import tempfile
+
 def handle(req):
     """handle a request to the function
     Args:
         req (request): flask request
     """
+    files = req.files
+    file_storages = files.getlist("files")
+    print(file_storages)
 
-    return req
+    merger = PdfFileMerger()
+
+    for file_storage in file_storages:
+        pdfFileReader = PdfFileReader(file_storage)
+        merger.append(pdfFileReader)
+
+    # with open('D:/result.pdf', 'wb') as fout:   # write binary
+    fout = tempfile.TemporaryFile()
+    merger.write(fout)
+
+    for file_storage in file_storages:
+        file_storage.close()
+
+    fout.seek(0)
+    return send_file(fout, 'application/pdf')
